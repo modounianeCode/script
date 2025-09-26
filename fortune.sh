7e87c4234ad1890690054f8bfada1ac9f92eae4c
Modou Niane <mniane2005@gmail.com>
2025-09-26 08:44:05 -0400

    Save progress: 1416.1

--- fortune.sh
+++ fortune.sh
@@ -4,17 +4,17 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 N=$((RANDOM % 6))
 echo ${RESPONSES[$N]}
-GET_FORTUNE(){
-  if [[ ! $1 ]]
-then
-  echo Ask a yes or no question:
-  read QUESTION
-else
-  echo Try again. Make sure it ends with a question mark:
-fi
-  read QUESTION
-} 
-until [[ $QUESTION =~ \?$ ]]
-do 
-   GET_FORTUNE again
+GET_FORTUNE() {
+  if [[ ! $1 ]]; then
+    echo "Ask a yes or no question:"
+    read QUESTION
+  else
+    echo "Try again. Make sure it ends with a question mark:"
+    read QUESTION
+  fi
+}
+
+GET_FORTUNE
+until [[ $QUESTION =~ \?$ ]]; do
+  GET_FORTUNE again
 done
