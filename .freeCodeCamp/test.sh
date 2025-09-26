379f1fce0bc8899f25f29b8ee0ef3ccebcf19bed
Modou Niane <mniane2005@gmail.com>
2025-09-26 03:52:19 -0400

    Save progress: 990.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 #Bingo Number Generator
 echo -e "\n~~ Bingo Number Generator ~~\n"
-NUMBER=$(( RANDOM % 15+46 ))
+NUMBER=$(( RANDOM % 15+61 ))
 TEXT="The next number is,"
 if (( NUMBER <= 15 ))
 then
   echo $TEXT B:$NUMBER
 elif [[ $NUMBER -le 30 ]]
 then
-echo $TEXT I:$NUMBER
+  echo $TEXT I:$NUMBER
 elif (( $NUMBER < 46 ))
 then
-echo $TEXT N:$NUMBER
+  echo $TEXT N:$NUMBER
 elif [[ $NUMBER -lt 61 ]]
 then
-echo $TEXT G:$NUMBER
+  echo $TEXT G:$NUMBER
+else
+  echo $TEXT 0:$NUMBER
+fi
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+elif [[ $NUMBER -le 30 ]]
+then
+  echo $TEXT I:$NUMBER
+elif (( NUMBER < 46 ))
+then
+  echo $TEXT N:$NUMBER
+elif [[ $NUMBER -lt 61 ]]
+then
+  echo $TEXT G:$NUMBER
+else
+  echo $TEXT O:$NUMBER
 fi
--- bingo.sh
+++ bingo.sh
@@ -8,11 +8,28 @@ then
   echo $TEXT B:$NUMBER
 elif [[ $NUMBER -le 30 ]]
 then
-echo $TEXT I:$NUMBER
+  echo $TEXT I:$NUMBER
 elif (( $NUMBER < 46 ))
 then
-echo $TEXT N:$NUMBER
+  echo $TEXT N:$NUMBER
 elif [[ $NUMBER -lt 61 ]]
 then
-echo $TEXT G:$NUMBER
+  echo $TEXT G:$NUMBER
+else
+  echo $TEXT 0:$NUMBER
+fi
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+elif [[ $NUMBER -le 30 ]]
+then
+  echo $TEXT I:$NUMBER
+elif (( NUMBER < 46 ))
+then
+  echo $TEXT N:$NUMBER
+elif [[ $NUMBER -lt 61 ]]
+then
+  echo $TEXT G:$NUMBER
+else
+  echo $TEXT O:$NUMBER
 fi
