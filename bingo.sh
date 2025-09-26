47b5b2b20773b0edb6cc73f47fb88e09d1441dcd
Modou Niane <mniane2005@gmail.com>
2025-09-26 03:37:12 -0400

    error:Your script should have the suggested if statement added correctly

--- bingo.sh
+++ bingo.sh
@@ -3,4 +3,8 @@
 echo -e "\n~~ Bingo Number Generator ~~\n"
 NUMBER=$(( RANDOM % 75 + 1 ))
 echo $NUMBER
-TEXT="The next number is, "
+TEXT="The next number is,"
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+fi
